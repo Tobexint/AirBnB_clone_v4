96d369908bc96aa616245483f1557558eefd347c
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-27 09:06:11 +0000

    dynamically style amenities div on check

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+$(function () {
+    function updateChecked() {
+	let checked = []
+	$("input:checked").each(function () {
+	    checked.push($(this).attr('data-name'));
+	});
+	return (checked);
+    }
+    $('input[type="checkbox"]').change(function () {
+	const checkedList = updateChecked();
+	let theList = "";
+	for (let i = 0; i < checkedList.length; i++) {
+	    console.log(checkedList[i]);
+	    theList = theList + checkedList[i];
+	    if (i < checkedList.length - 1)
+		theList = theList + ",";
+	}
+	$('div.amenities h4').text(theList);
+    });
+});
