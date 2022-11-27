2f6cee846041a5b6dafc0b036d94ce0a8c87cd06
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-27 11:58:17 +0000

    retrieve status visually to check availability

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+$(function () {
+    function updateChecked() {
+	let checked = []
+	$("input:checked").each(function () {
+	    checked.push($(this).attr('data-name'));
+	});
+	return (checked);
+    }
+    $('input[type="checkbox"]').change(function () {
+	const checkedList = updateChecked();
+	let theList = "";
+	for (let i = 0; i < checkedList.length; i++) {
+	    theList = theList + checkedList[i];
+	    if (i < checkedList.length - 1)
+		theList = theList + ",";
+	}
+	$('div.amenities h4').text(theList);
+    });
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, stat) {
+	if (data.status == "OK") {
+	    $("div#api_status").addClass("available");
+	}
+	else {
+	    $("div#api_status").removeClass("available");
+	}
+    })
+});
